b72c3d16ccc87f4475d1aa8994c92af4542ad7e0
linzehao <zehao.lin@ppiaas.cn>
2020-12-24 11:09:46 +0800

    update

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,29 +6,19 @@ import (
 	"testGinandGorm/common/mySQL_db"
 	"testGinandGorm/common/redis_utils"
 	"testGinandGorm/pkg/dao"
-	db2 "testGinandGorm/pkg/db"
+	"testGinandGorm/pkg/db"
 	"testGinandGorm/pkg/handler"
 	"testGinandGorm/pkg/router"
 	"testGinandGorm/pkg/service"
 )
 
-//func main() {
-//	Db := mySQL_db.DbInit()
-//	orderDao := dao.NewOrderDao(Db)
-//	orderService := service.NewService(orderDao)
-//	orderHander := handler.NewHandler(orderService)
-//	fmt.Println(orderHander)
-//	router.BindRoute(orderHander)
-//	mySQL_db.DbClose(Db)
-//}
-
 func main() {
-	db := mySQL_db.DbInit()
-	orderDB := db2.NewMyOrderDB(db)
+	sqlDB := mySQL_db.DbInit()
+	orderDB := db.NewMyOrderDB(sqlDB)
 	orderCache := redis_utils.NewRedisCache(1e10 * 6 * 20)
 	orderDao := dao.NewMyOrderDao(orderDB, &orderCache)
 	orderService := service.NewOrderService(orderDao)
 	ordeHandler := handler.NewOrderHandler(orderService)
 	router.BindRoute(ordeHandler)
-	db.Close()
+	sqlDB.Close()
 }
